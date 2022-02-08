297b83feb3b4d3a031f646ecfe9bc86d4a83725f
Chris Martin <chris.martin@oforce.com>
2022-02-08 01:04:17 -0700

    fix otp_app

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,3 +1,3 @@
 defmodule Cqrs.Repo do
-  use Ecto.Repo, otp_app: :cqrs_tools_bounded_context, adapter: Etso.Adapter
+  use Ecto.Repo, otp_app: :cqrs_tools_absinthe_relay, adapter: Etso.Adapter
 end
