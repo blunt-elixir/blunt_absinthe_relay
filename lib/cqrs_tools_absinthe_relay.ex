7bdb1737bd3b6536288f4f867b21a77fe740724e
Chris Martin <chris.martin@oforce.com>
2022-02-07 17:40:39 -0700

    shiny

---
+++ lib/cqrs_tools_absinthe_relay.ex
@@ -0,0 +1,18 @@
+defmodule CqrsToolsAbsintheRelay do
+  @moduledoc """
+  Documentation for `CqrsToolsAbsintheRelay`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CqrsToolsAbsintheRelay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cqrs_tools_absinthe_relay_test.exs
@@ -0,0 +1,8 @@
+defmodule CqrsToolsAbsintheRelayTest do
+  use ExUnit.Case
+  doctest CqrsToolsAbsintheRelay
+
+  test "greets the world" do
+    assert CqrsToolsAbsintheRelay.hello() == :world
+  end
+end
